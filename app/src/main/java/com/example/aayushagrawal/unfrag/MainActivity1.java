56558941809ad665641eaf28aaaa0b927164fc93
aayushagrawal135 <aayushagrawal135@gmail.com>
2016-07-10 01:48:58 +0530

    added weather-forecast

---
+++ app/src/main/java/com/example/aayushagrawal/unfrag/MainActivity1.java
@@ -0,0 +1,18 @@
+package com.example.aayushagrawal.unfrag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity1 extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main1);
+
+
+    }
+}
